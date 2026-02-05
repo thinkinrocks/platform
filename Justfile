2b077d35dc6b9bd269700e892871649f53a11f46
Artur Roos <artur.roos@ktnlvr.dev>
2026-02-05 18:58:35 +0200

    Initial commit

--- Justfile
+++ Justfile
@@ -1,10 +1,2 @@
 run:
     cargo run
-
-db:
-    sqlite3 sqlite.db ".read src/sql/setup.sql"
-
-demo-db:
-    rm -rf sqlite.db
-    just db
-    sqlite3 sqlite.db ".read samples/samples.sql"
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,7 @@
-mod bot;
-mod models;
-mod repository;
-mod templates;
-mod web;
-
-use sqlx::sqlite::SqlitePoolOptions;
-use tokio::task::JoinSet;
-
-use crate::{bot::serve_bot, repository::Repository, web::serve_web};
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().unwrap();
     pretty_env_logger::init();
-
-    let database_url = std::env::var("DATABASE_URL")?;
-
-    let pool = SqlitePoolOptions::new()
-        .max_connections(1)
-        .connect(&database_url)
-        .await?;
-
-    let repo = Repository::new(pool);
-
-    let mut join = JoinSet::new();
-
-    join.spawn(serve_bot(repo.clone()));
-    join.spawn(serve_web("0.0.0.0:3000", repo));
-
-    tokio::select! {
-        _ = tokio::signal::ctrl_c() => {
-            log::info!("Ctrl+C received, shutting down...");
-        }
-
-        res = join.join_next() => {
-            if let Some(res) = res {
-                if let Err(err) = res {
-                    log::error!("Task failed: {:?}", err);
-                }
-            }
-        }
-    }
-
-    join.abort_all();
-
-    while let Some(res) = join.join_next().await {
-        if let Err(err) = res {
-            log::debug!("Task aborted: {:?}", err);
-        }
-    }
-
-    log::info!("Shutdown complete");
 
     Ok(())
 }
